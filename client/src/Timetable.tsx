c69c7dfd82d61a260f94ae616750c25f70910e7c
Bence Darabos <makosbab@gmail.com>
2026-06-24 10:33:50 +0200

    Highlight next trip and dim past trips in Timetable

--- client/src/Timetable.tsx
+++ client/src/Timetable.tsx
@@ -1,12 +1,26 @@
 export const Timetable = ({ trips }) => {
+  const now = new Date().toLocaleTimeString("en-GB"); // ponytail: "HH:mm:ss" format
+  let nextFound = false;
+
   return (
     <div>
-      {trips.map((trip) => (
-        <>
-          <span>{trip.time}</span>
-          <br />
-        </>
-      ))}
+      {trips.map((trip) => {
+        const isPast = trip.time < now;
+        const isNext = !isPast && !nextFound;
+        if (isNext) nextFound = true;
+
+        return (
+          <div
+            key={trip.time}
+            style={{
+              color: isPast ? "lightgrey" : isNext ? "black" : "inherit",
+              fontWeight: isNext ? "bold" : "normal",
+            }}
+          >
+            {trip.time}
+          </div>
+        );
+      })}
     </div>
   );
 };
