93812e66bee4dc64e40f2c0c03999d20fd1254fa
Bence Darabos <makosbab@gmail.com>
2026-06-25 19:09:31 +0200

    Add PWA support
    Include a web manifest, service worker registration, and
    required assets to enable Progressive Web App functionality.

---
+++ client/public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+  console.log('Service worker installing...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Minimal fetch handler to satisfy PWA requirements
+  event.respondWith(fetch(event.request));
+});
